f634454a371d156860b06f8ca6484d4fc532db59
Telemet <telemetappb@gmail.com>
2020-04-29 13:12:33 +0300

    Add image

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
         <div class="relative pb-1/3">
           <img
             class="h-full w-full object-cover"
-            src="~/assets/images/lib/act-blackflag-long.jpg"
+            src="~/assets/images/lib/mc-madonna-eurovision-israel-2019.jpg"
           />
         </div>
       </div>
@@ -23,6 +23,69 @@
       />
     </div> -->
     <!-- Grid 3/1 -->
+    <div class="px-2 mt-4">
+      <div class="flex flex-wrap -mx-4">
+        <!-- Story 2 -->
+        <div class="sm:w-1/2 md:w-1/3 px-2 my-2">
+          <div class="relative pb-2/3">
+            <img
+              class="absolute h-full w-full object-cover"
+              src="~/assets/images/lib/mc-madonna-eurovision-israel-2019.jpg"
+            />
+          </div>
+          <div class="mb-4">
+            <h2 class="mt-3 text-xl font-bold">
+              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
+            </h2>
+            <p class="mt-2">
+              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
+              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
+              קבוע.
+            </p>
+          </div>
+        </div>
+        <!-- Story 3 -->
+        <div class="sm:w-1/2 md:w-1/3 px-2 my-2">
+          <div class="relative pb-2/3">
+            <img
+              class="absolute h-full w-full object-cover"
+              src="~/assets/images/telemet/telemet-poster.jpg"
+            />
+          </div>
+          <div class="mb-4">
+            <h2 class="mt-3 text-xl font-bold">
+              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
+            </h2>
+            <p class="mt-2">
+              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
+              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
+              קבוע.
+            </p>
+          </div>
+        </div>
+        <!-- Story 1 -->
+        <div class="sm:w-1/2 md:w-1/3 px-2 my-2">
+          <div class="relative pb-2/3">
+            <img
+              class="absolute h-full w-full object-cover"
+              src="~/assets/images/lib/mc-pp-london-2012-olympics-corona.jpg"
+            />
+          </div>
+          <div class="mb-4">
+            <h2 class="mt-3 text-xl font-bold">
+              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
+            </h2>
+            <p class="mt-2">
+              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
+              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
+              קבוע.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Grid 3/1 -->
 
     <div class="px-2 mt-4">
       <div class="flex flex-wrap -mx-4">
@@ -31,8 +94,18 @@
           <div class="relative pb-2/3">
             <img
               class="absolute h-full w-full object-cover"
-              src="~/assets/images/telemet/telemet-poster.jpg"
+              src="~/assets/images/lib/mc-madonna-eurovision-israel-2019.jpg"
             />
+          </div>
+          <div class="mb-4">
+            <h2 class="mt-3 text-xl font-bold">
+              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
+            </h2>
+            <p class="mt-2">
+              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
+              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
+              קבוע.
+            </p>
           </div>
         </div>
         <!-- Story 3 -->
@@ -40,8 +113,18 @@
           <div class="relative pb-2/3">
             <img
               class="absolute h-full w-full object-cover"
-              src="~/assets/images/lib/act-blackflag.jpg"
+              src="~/assets/images/telemet/telemet-poster.jpg"
             />
+          </div>
+          <div class="mb-4">
+            <h2 class="mt-3 text-xl font-bold">
+              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
+            </h2>
+            <p class="mt-2">
+              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
+              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
+              קבוע.
+            </p>
           </div>
         </div>
         <!-- Story 1 -->
@@ -51,6 +134,16 @@
               class="absolute h-full w-full object-cover"
               src="~/assets/images/lib/mc-pp-london-2012-olympics-corona.jpg"
             />
+          </div>
+          <div class="mb-4">
+            <h2 class="mt-3 text-xl font-bold">
+              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
+            </h2>
+            <p class="mt-2">
+              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
+              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
+              קבוע.
+            </p>
           </div>
         </div>
       </div>
