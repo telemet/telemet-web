3f39620dbc05e723d4f2f79360893745940b2bc9
Telemet <telemetappb@gmail.com>
2020-06-30 14:21:30 +0300

    Update main page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,43 @@
 <template>
   <!-- :dir="$dir()" -->
   <div
-    class="max-w-screen-xl px-0 md:px-8 lg:px-10 mx-auto min-h-screen bg-white"
+    class="min-h-screen bg-red-500 flex flex-col justify-between items-center"
   >
-    <app-header />
-    <section class="mx-auto">
-      <nuxt class="px-6 md:px-0" />
-    </section>
+    <!-- <app-header /> -->
+    <div class="flex-1"></div>
+    <div class="flex-1 text-center">
+      <span class="text-8xl font-black text-white">שעת חירום</span>
+      <!-- <nuxt class="px-6 md:px-0" /> -->
+    </div>
+    <div class="flex-2 mb-8 px-8 text-lg text-center font-bold">
+      עקב ״משבר הקורונה״ אנו נערכים גם ״לנורמלי החדש״ ונכנסים לתקנות שעת חירום,
+      משלנו. <br />פרטים בקרוב
+      <a
+        href="https://t.me/telemetISR"
+        title="קבוצת טלאמת בטלגרם"
+        class="text-white hover:text-black"
+        target="_blank"
+        >בטלגרם</a
+      >
+      <a
+        href="https://www.facebook.com/groups/telemetISR/"
+        title="קבוצת טלאמת בפייסבוק"
+        class="text-white hover:text-black"
+        target="_blank"
+        >ובפייסבוק</a
+      ><br />
+      <AppLogotype class="m-0 p-0 absolute left-0 top-0" />
+    </div>
   </div>
 </template>
 
 <script>
-import AppHeader from '@/components/AppHeader'
+import AppLogotype from '@/components/AppLogotype'
+// import AppHeader from '@/components/AppHeader'
 export default {
   components: {
-    AppHeader
+    AppLogotype
+    // AppHeader
   },
   // computed: {
   //   currentLanguage() {
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,8 @@
   <div class="min-h-screen mx-auto">
     <!-- Hero -->
     <div
-      class="mt-4 w-full h-96 bg-black flex justify-center items-center overflow-hidden"
-    >
-      <!-- Story 3 -->
-      <div class="w-full">
-        <div class="relative pb-1/3">
-          <img
-            class="h-full w-full object-cover"
-            src="~/assets/images/lib/mc-madonna-eurovision-israel-2019.jpg"
-          />
-        </div>
-      </div>
-    </div>
+      class="mt-4 w-full h-96 bg-red-500 flex justify-center items-center overflow-hidden"
+    ></div>
 
     <!-- <div class="bg-brand-400 relativepb-2/3">
       <img
@@ -22,132 +12,6 @@
         src="~/assets/images/telemet/telemet-poster.jpg"
       />
     </div> -->
-    <!-- Grid 3/1 -->
-    <div class="px-2 mt-4">
-      <div class="flex flex-wrap -mx-4">
-        <!-- Story 2 -->
-        <div class="sm:w-1/2 md:w-1/3 px-2 my-2">
-          <div class="relative pb-2/3">
-            <img
-              class="absolute h-full w-full object-cover"
-              src="~/assets/images/lib/mc-madonna-eurovision-israel-2019.jpg"
-            />
-          </div>
-          <div class="mb-4">
-            <h2 class="mt-3 text-xl font-bold">
-              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
-            </h2>
-            <p class="mt-2">
-              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
-              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
-              קבוע.
-            </p>
-          </div>
-        </div>
-        <!-- Story 3 -->
-        <div class="sm:w-1/2 md:w-1/3 px-2 my-2">
-          <div class="relative pb-2/3">
-            <img
-              class="absolute h-full w-full object-cover"
-              src="~/assets/images/telemet/telemet-poster.jpg"
-            />
-          </div>
-          <div class="mb-4">
-            <h2 class="mt-3 text-xl font-bold">
-              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
-            </h2>
-            <p class="mt-2">
-              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
-              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
-              קבוע.
-            </p>
-          </div>
-        </div>
-        <!-- Story 1 -->
-        <div class="sm:w-1/2 md:w-1/3 px-2 my-2">
-          <div class="relative pb-2/3">
-            <img
-              class="absolute h-full w-full object-cover"
-              src="~/assets/images/lib/mc-pp-london-2012-olympics-corona.jpg"
-            />
-          </div>
-          <div class="mb-4">
-            <h2 class="mt-3 text-xl font-bold">
-              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
-            </h2>
-            <p class="mt-2">
-              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
-              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
-              קבוע.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Grid 3/1 -->
-
-    <div class="px-2 mt-4">
-      <div class="flex flex-wrap -mx-4">
-        <!-- Story 2 -->
-        <div class="w-1/3 px-2 my-2">
-          <div class="relative pb-2/3">
-            <img
-              class="absolute h-full w-full object-cover"
-              src="~/assets/images/lib/mc-madonna-eurovision-israel-2019.jpg"
-            />
-          </div>
-          <div class="mb-4">
-            <h2 class="mt-3 text-xl font-bold">
-              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
-            </h2>
-            <p class="mt-2">
-              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
-              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
-              קבוע.
-            </p>
-          </div>
-        </div>
-        <!-- Story 3 -->
-        <div class="w-1/3 px-2 my-2">
-          <div class="relative pb-2/3">
-            <img
-              class="absolute h-full w-full object-cover"
-              src="~/assets/images/telemet/telemet-poster.jpg"
-            />
-          </div>
-          <div class="mb-4">
-            <h2 class="mt-3 text-xl font-bold">
-              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
-            </h2>
-            <p class="mt-2">
-              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
-              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
-              קבוע.
-            </p>
-          </div>
-        </div>
-        <!-- Story 1 -->
-        <div class="w-1/3 px-2 my-2">
-          <div class="relative pb-2/3">
-            <img
-              class="absolute h-full w-full object-cover"
-              src="~/assets/images/lib/mc-pp-london-2012-olympics-corona.jpg"
-            />
-          </div>
-          <div class="mb-4">
-            <h2 class="mt-3 text-xl font-bold">
-              מדונה, טקס האירויזיון, ונגיף הקורונה כל מה שרציתם לדעת
-            </h2>
-            <p class="mt-2">
-              ההגנה הטובה ביותר נגד טיפות אלו היא הימנעות מנגיעה בפנים, חיטוי
-              במשטחי כל הפריטים שאתה נוגע בהם, ושטוף או מחטא את הידיים באופן
-              קבוע.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -224,11 +88,6 @@ export default {
 </script>
 
 <style lang="postcss">
-.grid-item {
-  @apply bg-gray-400 h-12 px-2 w-1/3;
-  /* width: 33.333%; */
-}
-
 /*  {
   padding-bottom: 66.66666%;
 } */
